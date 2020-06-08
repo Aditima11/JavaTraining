0bc2fc34fb1415163734ec81524fe6caefd8d80f
Aditima Patnaik <aditimapatnaik@Aditimas-MacBook-Pro.local>
2020-06-08 11:13:30 +0530

    This is my first commit

---
+++ src/main/java/life/circles/Packagae1/FirstIntellijJavaProgram.java
@@ -0,0 +1,8 @@
+package life.circles.Packagae1;
+
+public class FirstIntellijJavaProgram
+{ 
+    public static void main(String [] a){ 
+        System.out.println("This is first java program");     } 
+
+}
